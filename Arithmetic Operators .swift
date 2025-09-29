c2aff3f5fd11b057feaefcf1afeb074b44b1050b
S3an-1 <seanrv61@gmail.com>
2025-09-28 23:41:47 -0500

    Add files via upload

---
+++ Arithmetic Operators .swift
@@ -0,0 +1,36 @@
+// + Addition
+// - Subtraction
+// * Multiplication
+// / Division
+
+// Operations with numbers
+// (+) Operations for Addition
+let sum = 23 + 20
+
+//(-) Operatior for Subtraction
+let result = 32 - sum
+
+// Operatior for Multiplication
+let total = result * 5
+// (/) Operatior for Division
+let divide = total / 10
+print(sum)
+print(result)
+print(total)
+print(divide)
+
+let a = 12
+let b = 12.0
+//let c = a + b
+let c = Double(a) + b
+print(c)
+
+var aa = 1 
+
+print(aa)
+
+aa += 2
+
+print(aa)
+
+aa -= 1
